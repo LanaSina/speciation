99f57df74e1198a68c7c966912080227db553550
Théo De Pinho <theo.depinho.etu@univ-lille.fr>
2025-01-23 21:31:09 +0100

    ShadowIndividualTest: simplify shadowIndividualIsIdenticalToTheOriginalIndividualAtCreation()

--- tests/animals/ShadowIndividualTest.java
+++ tests/animals/ShadowIndividualTest.java
@@ -22,28 +22,7 @@ public class ShadowIndividualTest {
         IndividualWithProperties individual = new EmbodiedIndividual(x, y, id, firstAncestorID, birthDate, parentID);
         ShadowIndividual shadowIndividual = new ShadowIndividual(individual);
 
-        assertEquals(individual.cellTransparency, shadowIndividual.cellTransparency);
-        assertEquals(individual.getSpeed(), shadowIndividual.getSpeed());
-        assertEquals(individual.getMaxEnergy(), shadowIndividual.getMaxEnergy());
-        assertEquals(individual.getKidEnergy(), shadowIndividual.getKidEnergy());
-        assertEquals(individual.getMatForKids(), shadowIndividual.getMatForKids());
-        assertEquals(individual.getNKids(), shadowIndividual.getNKids());
-        assertEquals(individual.getDeath(), shadowIndividual.getDeath());
-
-//        assertEquals(individual.getSensors(), shadowIndividual.getSensors()); // does not work: no method equals() in Tree
-        assertEquals(id, shadowIndividual.getID());
-        assertEquals(parentID, shadowIndividual.getParentID());
-        assertEquals(firstAncestorID, shadowIndividual.getFirstAncestorID());
-        assertEquals(birthDate, shadowIndividual.getBirthDate());
-
-        assertEquals(individual.getLifeSpan(), shadowIndividual.getLifeSpan());
-        assertEquals(individual.isLight(), shadowIndividual.isLight());
-
-        assertEquals(individual.getEnergy(), shadowIndividual.getEnergy());
-        assertEquals(x, shadowIndividual.getPosition()[0]);
-        assertEquals(y, shadowIndividual.getPosition()[1]);
-        assertEquals(individual.color, shadowIndividual.color);
-        assertEquals(individual.borderColor, shadowIndividual.borderColor);
+        assertEquals(individual, shadowIndividual);
 
     }
 
