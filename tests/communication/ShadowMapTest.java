2081c7ea6400d6f3cf5c885dbe7ba76d0a67999c
Théo De Pinho <theo.depinho.etu@univ-lille.fr>
2025-01-24 15:40:03 +0100

    ShadowMapTest: add checkWhetherTheNumberOfRandomlyRemovedIndividualsIsCorrect()

--- tests/communication/ShadowMapTest.java
+++ tests/communication/ShadowMapTest.java
@@ -140,4 +140,23 @@ public class ShadowMapTest {
         assertEquals(number, shadowMap.babies.size());
     }
 
+    @Test
+    public void checkWhetherTheNumberOfRandomlyRemovedIndividualsIsCorrect() {
+        ShadowMap shadowMap = new ShadowMap(realMap, SHADOW_DISPLAY, SHADOW_SUMMARY_FILE_NAME, SHADOW_PREDATION_FILE_NAME, SHADOW_SNAPSHOT_FILE_NAME, SHADOW_SENSORS_FILE_NAME);
+        int x = 20;
+        int y = 20;
+        int globalID = 30;
+        int ancestor = 12;
+        int date = 800;
+        int parent = 2;
+        int number = 10;
+        for (int i = 0 ; i < number ; i++) {
+            ShadowIndividual individual = new ShadowIndividual(x, y, globalID++, ancestor++, date++, parent++);
+            shadowMap.addIndividual(x++, y++, individual);
+        }
+        assertEquals(0, shadowMap.remove.size());
+        shadowMap.removeRandomIndividuals(number);
+        assertEquals(number, shadowMap.remove.size());
+    }
+
 }
